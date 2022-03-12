0c41757b0affeac37aca4e37159bcfce8328b4ce
Adilbaev Ruslan <adilbaev-ruslan@umail.uz>
2022-03-12 21:12:23 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
 	$telegram = new Telegram('5192813902:AAFlywjK7TjiZyDLglrH6Kb40RvFa3YuiNI');
 
 	$chat_id = $telegram->ChatID();
-	$content = array('chat_id' => $chat_id, 'text' => 'Test');
+
+	$text = $telegram->Text();
+
+	$content = array('chat_id' => $chat_id, 'text' => $text);
 	$telegram->sendMessage($content);
 	
  ?>
