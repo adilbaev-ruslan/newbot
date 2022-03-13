67eb4c903828a3885ebc3276fa4c4a744f6a415e
Adilbaev Ruslan <adilbaev-ruslan@umail.uz>
2022-03-13 11:59:35 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -34,17 +34,26 @@ if ($text == "/start") {
     $keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize=true, $selective=true);
 	$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Буйыртпаны сайлан");
 	$telegram->sendMessage($content);
+} elseif ($text = "1 Кг - 100 000,0 сум") {
+	askMessage();
+} elseif ($text = "2 Кг - 200 000,0 сум") {
+	askMessage();
+} elseif ($text = "3 Кг - 300 000,0 сум") {
+	askMessage();
+} elseif ($text = "4 Кг - 400 000,0 сумs") {
+	askMessage();
 }
-// function askMessage() {
-// 	global $telegram, $chat_id;
-// 	$option = array(
-//     	array(
-//     		$telegram->buildKeyboardButton("Сиздин телефон номерининз", ture),
-//     	),
-//     );
-//     $keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize=true, $selective=true);
-// 	$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Буйыртпа бериу ушын телефон номерининизди киритин");
-// 	$telegram->sendMessage($content);
-// }
+
+function askMessage() {
+	global $telegram, $chat_id;
+	$option = array(
+    	array(
+    		$telegram->buildKeyboardButton("Сиздин телефон номерининз", $request_contact = true),
+    	),
+    );
+    $keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize=true, $selective=true);
+	$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Буйыртпа бериу ушын телефон номерининизди киритин");
+	$telegram->sendMessage($content);
+}
 	
- ?>
+?>
